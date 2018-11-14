9255bb20f1e3cb3f46a7b8bbc7867eeae8abaf9f
kspolyantseva <ks.polyantseva@yandex.ru>
2018-11-14 11:03:03 +0300

    Initial Commit

---
+++ lab1_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab1_app
+//
+//  Created by Ksenia Polyantseva on 14/11/2018.
+//  Copyright © 2018 Ксения Полянцева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
